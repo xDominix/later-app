bb05ab0f6924e26acc751bedc499b7c3799144cd
Dominik J <dominikj@student.agh.edu.pl>
2022-12-03 15:07:44 +0100

    Updates

---
+++ precache-manifest.b1acffa1a6f1a983bd244f1319f7c5ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe2c9938d01521acae55",
+    "url": "/later-app/static/css/main.d8c33b0c.chunk.css"
+  },
+  {
+    "revision": "fe2c9938d01521acae55",
+    "url": "/later-app/static/js/main.3382ef4e.chunk.js"
+  },
+  {
+    "revision": "756967bf4f76d9881254",
+    "url": "/later-app/static/js/runtime~main.97df5dc1.js"
+  },
+  {
+    "revision": "f844da4cce7aee28afba",
+    "url": "/later-app/static/js/2.9e3599a7.chunk.js"
+  },
+  {
+    "revision": "5b3098a0d13a72d6858600ae183fa047",
+    "url": "/later-app/index.html"
+  }
+];
